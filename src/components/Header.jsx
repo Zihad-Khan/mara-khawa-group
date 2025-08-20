70456a4393c500dfe9d24f03caf6d2f855e004f8
Md. Murshid Khan Zihad <mkzihad307@gmail.com>
2025-08-20 18:19:47 +0600

    fixed some mobile bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="navbar bg-rose-600 fixed shadow-sm justify-center">
         <div className="navbar-center">
-            <h2 className="text-2xl center font-bold">THE PROCHUR MARA KHAWA GROUP</h2>
+            <h2 className="text-2xl center font-bold">THANK YOU</h2>
         </div>
     </div>
   )
